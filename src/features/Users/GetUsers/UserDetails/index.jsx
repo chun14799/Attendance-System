deb05a0165f57b2de936d7429e02e8e8ae9f08ac
HoangHiep <hoanghiep@HoangHiepnoMacBook-puro.local>
2020-12-28 01:32:00 +0700

    temp push

--- src/features/Users/GetUsers/UserDetails/index.jsx
+++ src/features/Users/GetUsers/UserDetails/index.jsx
@@ -9,12 +9,19 @@ function UserDetails({user}) {
     return (
         <div>
             <ul className="user-info">
-                <li>{user.id}</li>
+                <li>{user._id}</li>
                 <li className="user-fullName">{user.fullName}</li>
-                <li className="user-image">{user.image}</li>
+                <li className="user-image">{user.imgPath}</li>
                 <li className="user-pin">{user.pin}</li>
-                <li className="user-dob">{user.email}</li>
+                <li className="user-dob">{user.dob}</li>
                 <li className="user-homeAddress">{user.homeAddress}</li>
+                <li className="user-grossSalary">{user.grossSalary}</li>
+                <li className="user-netSalary">{user.netSalary}</li>
+                <li className="user-note">{user.note}</li>
+                <li className="user-departments">{user.departments}</li>
+                <li className="user-roles">{user.roles}</li>
+                <li className="user-shifts">{user.shifts}</li>
+
                 <ul className="btn-edit-delete">
                     <li><a href="/users">Sửa</a></li>
                     <li><a href="/users">Xóa</a></li>
--- src/features/Users/GetUsers/index.jsx
+++ src/features/Users/GetUsers/index.jsx
@@ -1,51 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState} from 'react';
 import UserList from './UserDetails/UserList';
 import './style.scss';
 import Header from '../../../components/Header';
 import InfoBar from '../../../components/InfoBar';
 import userApi from '../../../api/userApi';
 
-const listUser =[
-    {
-        id: 1,
-        name: "Vũ Hoàng Hiệp",
-        office: "SPKT",
-        chucvu: "Sinh Viên",
-        email: "hiepvu@gmail.com",
-        sdt: "0903113114",
-    },
-    {
-        id: 2,
-        name: "Lê Hồng Danh",
-        office: "SPKT",
-        chucvu: "Sinh Viên",
-        email: "danhle@gmail.com",
-        sdt: "0901234567",
-    },
-    {
-        id: 3,
-        name: "Đặng Huỳnh Hoàng Long",
-        office: "SPKT",
-        chucvu: "Sinh Viên",
-        email: "longdang@gmail.com",
-        sdt: "090876543",
-    },
-];
 function GetUser(props) {
-    useEffect(()=>{
-        const fetchUsers = async()=>{
-            const userList = await userApi.GetAll();
-            console.log(userList);
+    const [data, setData] = useState([]);
+
+    useEffect(() =>{
+        const fetchData = async ()=>{
+            const result = await userApi.GetAll();
+            setData(result);
         };
-        fetchUsers();
-    },[]);
-    
+        fetchData();
+    }, []);
+
     return (
         <div>
             <Header/>
             <h2 className="status">User</h2>
             <InfoBar/>
-            <UserList listUser={listUser}/>
+            <UserList listUser={data}/>
         </div>
     );
 }
